7ec3cad73751af4247c359337f531d66198ab8d6
Vijendra Boipai <vijendraboipai22@gmail.com>
2020-08-08 00:33:58 +0530

    There is new modified file added

---
+++ Scanner.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Danger{
+	public static void main(String args[]) {
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("enter the value...");
+
+		String line = input.nextLine();
+
+		System.out.println("your entered value :"+ line);
+
+		System.out.println("This is just for a practice purpose....git hub");
+		System.out.println("again adding something for the demo purpose");
+
+	}
+		
+}
+
+/*
+
+import java.util.Scanner;
+class Oreo {
+	public static void main(String args[]){
+	
+		Scanner input = new Scanner(System.in);
+	
+		System.out.println("enter the you wanna enter..");
+
+		int Value = input.nextInt();
+	
+		System.out.println("the value which you hava entered :" + Value);
+
+	}
+
+}
+*/
